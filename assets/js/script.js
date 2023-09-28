f9933e67e895192a5af65b26dba5abaa9a8350c6
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-28 14:23:38 -0400

    continuing test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     "https://cors.io/?https://blockchain.info/stats?format=json",
     function (data) {
       var url =
-        "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=2BFE828F4B1A7B8EA83398AA3E97E9EB&steamid=76561197960435530&relationship=friend";
+        "https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=2BFE828F4B1A7B8EA83398AA3E97E9EB&steamid=76561197960435530&relationship=friend";
       $.ajax({
         url: url,
         dataType: "json",
