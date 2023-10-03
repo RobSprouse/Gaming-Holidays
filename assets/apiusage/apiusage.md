9ee949bda80248f433c9e6006c7d58728dc27bf4
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-10-02 20:37:11 -0400

    added mockup/normalize file, little changes to other files

---
+++ assets/apiusage/apiusage.md
@@ -0,0 +1,21 @@
+Name 
+
+https://api.rawg.io/api/games/{id}.name
+
+
+platforms
+
+https://api.rawg.io/api/games/{id}.platforms.name
+
+Developers
+
+https://api.rawg.io/api/games/{id}.developers
+
+
+release date
+https://api.rawg.io/api/games/{id}.released
+
+fun fact date
+
+(background-image)
+https://api.rawg.io/api/games/{id}.background_image
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-fetch("https://api.rawg.io/api/platforms?key=164d87e9b2364003ad69bc496d5e3d7f")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
 
-fetch("https://api.rawg.io/api/platforms?key=164d87e9b2364003ad69bc496d5e3d7f")
-  .then((response) => response.json())
-  .then((data) => {
-    // Select the element where you want to display the data
-    const element = document.getElementById("output");
 
-    // Iterate over the data and create HTML for each item
-    data.results.forEach((item) => {
-      const itemElement = document.createElement("div");
-      itemElement.textContent = `Platform: ${item.name}, Games Count: ${item.games_count}`;
-      element.appendChild(itemElement);
-    });
-  })
-  .catch((error) => console.error("Error:", error));
+
+
+fetch("https://api.rawg.io/api/games?key=164d87e9b2364003ad69bc496d5e3d7f")
+     .then((response) => response.json())
+     .then((data) => console.log(data))
+     .catch((error) => console.error("Error:", error));
+
+// fetch("https://api.rawg.io/api/platforms?key=164d87e9b2364003ad69bc496d5e3d7f")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // Select the element where you want to display the data
+//     const element = document.getElementById("output");
+
+//     // Iterate over the data and create HTML for each item
+//     data.results.forEach((item) => {
+//       const itemElement = document.createElement("div");
+//       itemElement.textContent = `Platform: ${item.name}, Games Count: ${item.games_count}`;
+//       element.appendChild(itemElement);
+//     });
+//   })
+//   .catch((error) => console.error("Error:", error));
+
