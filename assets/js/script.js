ae3062e7b13936487005e16be0e5523ff88b73b3
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-10-03 23:40:44 -0400

    mockup.drawio, script.js, apiusage.md
    # mockup
    
    struck out a value we couldn't grab from the fetch games URL
    
    # apiusage
    
    added some more reference links
    
    # script.js
    
    compiled a list of where the keys are located when fetching gameListURL, gameIdURL, need to check if more is needed
    
    defined URLS that may/may not be used to fetch data
    
    defined search parameters when at least fetching with gameListURL, need to see what can be utilized for other search parameters and group them by such
    
    the fetch functions at the bottom are there for testing purposes, looking at the data response to determine/confirm the defined locations are correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,99 @@
+$(document).ready(function () {
+     // TODO: add event listeners
+});
 
+// TODO: Define all URL's and object keys
+const apiKey = "?key=164d87e9b2364003ad69bc496d5e3d7f";
 
+let gameName = "";
+let gameId = "";
+let genreId = "";
+let developerId = "";
+let platformId = "";
+let pageNum = "";
+let pageSize = "";
+let searchQuery = "";
+let searchOrder = "";
+let platforms = "";
+let developers = "";
+let genres = "";
+let tags = "";
+let releaseDates = "";
+let metacritic = "";
 
+// COMMENT: Game list and their data.keys commented
+const gameListURL = "https://api.rawg.io/api/games" + apiKey;
+// after fetchGameListURL (gameList)
+// gameID = gameList.results[i].id
+// gameName = gameList.results[i].name
+// platforms = gameList.results[i].platforms[j].platform.name
+// releasedDate = results[i].released
+// backgroundImg = results[i].background_image // (value is a URL)
 
+// COMMENT: Game and their data.keys commented
+const gameIdURL = "https://api.rawg.io/api/games/" + gameId + apiKey;
+// after fetchGameIdURL (game)
+// let gameName = game.name;
+// let gameDeveloper = game.developers;
+// let gameReleaseDate = game.released;
+// let gameBackgroundImage = game.background_image;
+// let gamePlatforms = game.platforms.name;
+
+// COMMENT: Genre
+const GenreURL = "https://api.rawg.io/api/genres" + apiKey;
+const GenreIdURL = "https://api.rawg.io/api/genres/" + genreId + apiKey;
+// COMMENT: Developers
+const DevelopersURL = "https://api.rawg.io/api/developers" + apiKey;
+const DeveloperIdURL = "https://api.rawg.io/api/developers/" + developerId + apiKey;
+// COMMENT: Platforms
+const PlatformsURL = "https://api.rawg.io/api/platforms" + apiKey;
+const PlatformIdURL = "https://api.rawg.io/api/platforms/" + platformId + apiKey;
+
+// COMMENT: Search Parameters for games
+const pageNumParam = `&page=${pageNum}`;
+const resultsPerPageParam = `&page_size=${pageSize}`;
+const searchQueryParam = `&search=${searchQuery}`;
+const platformsFilter = `&platforms=${platforms}`; // Filter by platforms, for example: 4,5.
+const developersFilter = `&developers=${developers}`; // Filter by developers, for example: 1612,18893 or valve-software,feral-interactive.
+const genresFilter = `&genres=${genres}`; // Filter by genres, for example: 4,51 or action,indie.
+const tagsFilter = `&tags=${tags}`; // Filter by tags, for example: 31,7 or singlePlayer,multiplayer.
+const datesFilter = `&dates=${releaseDates}`; // Filter by a release date, for example: 2010-01-01,2018-12-31.1960-01-01,1969-12-31.
+const metacriticFilter = `&metacritic=${metacritic}`; //Filter by a metaCritic rating, for example: 80,100.
+const searchOrderParam = `&ordering=${searchOrder}`; // Available fields: name, released, added, created, updated, rating, metacritic. You can reverse the sort order adding a hyphen, for example: -released.
+
+// fetch for gamesListURL
 fetch("https://api.rawg.io/api/games?key=164d87e9b2364003ad69bc496d5e3d7f")
-     .then((response) => response.json())
-     .then((data) => console.log(data))
+     .then((response) => {
+          if (!response.ok) {
+               $("#invalidEntry").show();
+               $("#invalidEntry").text("The response from the weather api failed. Please try again.");
+               throw new Error("HTTP error " + response.status);
+          }
+          return response.json();
+     })
+     .then((gameList) => {
+          console.log(gameList);
+          // TODO: create separate <div>'s with class="gameList", structure each to contain the contents within mock-up
+          // backgroundImg = results[i].background_image // (value is a URL)
+          // gameName = gameList.results[i].name
+          // platforms = gameList.results[i].platforms[j].platform.name
+          // releasedDate = results[i].released
+          // gameID = gameList.results[i].id // used to pass to fetchGameIdURL, not displayed
+     })
      .catch((error) => console.error("Error:", error));
 
-// fetch("https://api.rawg.io/api/platforms?key=164d87e9b2364003ad69bc496d5e3d7f")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // Select the element where you want to display the data
-//     const element = document.getElementById("output");
-
-//     // Iterate over the data and create HTML for each item
-//     data.results.forEach((item) => {
-//       const itemElement = document.createElement("div");
-//       itemElement.textContent = `Platform: ${item.name}, Games Count: ${item.games_count}`;
-//       element.appendChild(itemElement);
-//     });
-//   })
-//   .catch((error) => console.error("Error:", error));
-
+// fetch for game by id
+fetch("https://api.rawg.io/api/games/3498?key=164d87e9b2364003ad69bc496d5e3d7f")
+     .then((response) => {
+          if (!response.ok) {
+               $("#invalidEntry").show();
+               $("#invalidEntry").text("The response from the weather api failed. Please try again.");
+               throw new Error("HTTP error " + response.status);
+          }
+          return response.json();
+     })
+     .then((game) => {
+          console.log(game);
+          // TODO: creat a <div> with class "gameData". structure it to contain the contents the way we want
+     })
+     .catch((error) => console.error("Error:", error));
