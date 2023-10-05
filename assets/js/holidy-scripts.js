32c0fc5dcbc0dfa5136fc2aa1103400f435c8368
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-10-04 20:07:26 -0400

    add buttons that will allow user to override the date and year inputs

--- assets/js/holidy-scripts.js
+++ assets/js/holidy-scripts.js
@@ -7,18 +7,31 @@ var apiKey = '6dc87409-6cac-4849-ad17-e0f4fc8a6deb'
 
 // Xpert assistant help with setting up API 
 var holidaysList = document.getElementById("test-me");
-
-
+var searchButton = document.querySelector("#search-button");
+/*
 
 const url = `https://holidayapi.com/v1/holidays?pretty&key=6dc87409-6cac-4849-ad17-e0f4fc8a6deb&country=US&year=2022`;
 console.log(url)
+
 fetch(url)
   .then(response => response.json())
   .then(data => {
     // Process the response data
-    console.log(data);
+    holidayData = data
+
+    console.log(holidayData.holidays);
   })
   .catch(error => {
     // Handle any errors
     console.error(error);
   });
+*/
+
+function getCountry(){
+countryInput = document.getElementById("country-input").value;
+yearInput = document.getElementById("year-input").value;
+console.log(countryInput);
+console.log(yearInput);
+}
+
+searchButton.addEventListener("click", getCountry);
